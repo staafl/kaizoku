7c8433ca5390ce5f1d1f97f8679589829241c043
Arshad Chummun <arshad@chummun.com>
2014-07-23 23:29:24 +0400

    Added search and download command

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 #! /usr/bin/env node
 var program = require('commander');
-var subdb = require('./lib/kaizoku.js');
+var kaizoku = require('./lib/kaizoku.js');
 
 // Get version from package.json.
 var version = require('./package.json').version;
+program.version(version)
 
+/**
+ * Search command.
+ * Example: kaizoku search [keywords].
+ */
 program
-  .version(version)
+  .command('search [keywords]')
+  .description('Use this command to search torrents.')
+  .action(function(keywords, options){
+    if (!keywords) {
+      console.log('Error: keywords missing');
+      program.help();
+    }
+
+    kaizoku.search(keywords);
+  });
+
+/**
+ * Download command.
+ * Example: kaizoku download [keywords].
+ */
+program
+  .command('download [keywords]')
+  .description('Use this command to quickly download a torrent.')
+  .action(function(keywords, options){
+    if (!keywords) {
+      console.log('Error: keywords missing');
+      program.help();
+    }
+
+    kaizoku.download(keywords);
+  });
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
